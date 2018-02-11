740ee4cef6d9addc660d1e3b42d12f29df704aa3
Joel Sequeira <joelseq96@gmail.com>
2018-02-10 16:16:38 -0800

    Add scroll animation

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
   $(".mobile-link").click(function() {
     $(".df-nav").toggleClass("nav-open");
   });
+
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
 });
