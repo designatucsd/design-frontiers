feb243447f0f13b1c5b18cb82b3328c32f7a858d
Joel Sequeira <joelseq96@gmail.com>
2018-02-11 22:13:20 -0800

    Add chevrons for FAQs

--- js/main.js
+++ js/main.js
@@ -12,4 +12,12 @@ $(document).ready(function() {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
   });
+
+  function toggleChevron(e) {
+    const prev = $(e.target).prev();
+    prev.find('i.indicator').toggleClass('fa-chevron-down fa-chevron-up');
+  }
+
+  $('.df__faq').on('hidden.bs.collapse', toggleChevron);
+  $('.df__faq').on('shown.bs.collapse', toggleChevron);
 });
